1574908c66378c0d733072c9d443505981d9aff2
Jamie Nolan <42931085+jamieNolanDEV@users.noreply.github.com>
2021-03-23 11:21:22 +0000

    Cyclic Rotation

--- main.java
+++ main.java
@@ -18,16 +18,21 @@
 
 public class main {
     public static void main(String[] args) {
-
         int[] array = {1,2,3,4,5};
         int rotate = 1;
+        cycleArray(array,rotate);
+
+    }
+
+    public static void cycleArray(int[] array, int rotate){
+
 
         for (int i = 0; i < rotate; i++) {
-        int j, last;
-        last = array[array.length-1];
+            int j, last;
+            last = array[array.length-1];
 
             for (int k = array.length - 1; k > 0; k--) {
-            array[k] = array[k-1];
+                array[k] = array[k-1];
             }
             array[0] = last;
 
@@ -36,6 +41,7 @@ public class main {
         for(int i = 0; i< array.length; i++){
             System.out.print(array[i] + " ");
         }
+
     }
 }
 
